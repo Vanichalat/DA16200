047b6a68a314d149891ee5cf043b8f9e480a74a3
Vanichalat <vani.c@infolitz.com>
2023-10-30 18:33:05 +0530

    initial commit

---
+++ build_1/echo_bot_1/image_preload.cmake
@@ -0,0 +1,8 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf52840dk_nrf52840" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "c:/VANI/NRF/echo_bot_1/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CMAKE_VERBOSE_MAKEFILE "FALSE" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "C:/VANI/NRF/echo_bot_1/nrf52840dk_nrf52840.overlay" CACHE INTERNAL "NCS child image controlled")
+set(SYSBUILD "True" CACHE INTERNAL "NCS child image controlled")
+set(SYSBUILD_CACHE "C:/VANI/NRF/echo_bot_1/build_1/echo_bot_1_sysbuild_cache.txt" CACHE INTERNAL "NCS child image controlled")
